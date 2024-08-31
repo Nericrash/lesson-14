23b5929ec9628ae96d43377d73ace5930800cf19
YakovIsikeev <Barcas48ru@gmail.com>
2024-08-31 18:30:01 +0300

    add 15_1 lesson

--- src/category.py
+++ src/category.py
@@ -1,3 +1,8 @@
+from typing import Any
+
+from src.product import Product
+
+
 class Category:
     """Категория товара"""
 
@@ -11,6 +16,28 @@ class Category:
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
         Category.category_count += 1
         Category.product_count += len(products)
+        print(Category.product_count)
+
+    def __str__(self):
+        return f"{self.name}, количество продуктов: {len(self.__products)} шт."
+
+    def add_product(self, product: Product) -> Any:
+        self.__products.append(product)
+        Category.product_count += 1
+
+    @property
+    def get_product_list(self) -> str:
+        product_list = ""
+        for product in self.__products:
+            product_list += f"{str(product)}\n"
+        return product_list
+
+    @property
+    def products(self) -> list:
+        products_list = []
+        for product in self.__products:
+            products_list.append(product)
+        return products_list
